cf4b0f1eb631aa1e2e0dd08c36b7e3d2b7051316
Aarij <aarijb@outlook.com>
2026-04-24 15:30:04 +0100

    challenge 10

--- queries/challenge_10.sql
+++ queries/challenge_10.sql
@@ -4,3 +4,13 @@
 
 -- Results should be sorted alphabetically by category.
 
+select category_name, product_name
+from (
+    select category_name, product_name,
+           row_number() over (partition by category_name order by unit_price desc) as price_rank
+    from products
+    join categories using (category_id)
+) ranked_products
+where price_rank = 3
+order by category_name;
+
