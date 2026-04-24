9264f69f88252cac4a7eaca78267e35cdcafe85c
Aarij <aarijb@outlook.com>
2026-04-24 14:49:57 +0100

    challenge 3

--- queries/challenge_3.sql
+++ queries/challenge_3.sql
@@ -2,3 +2,8 @@
 
 -- Results should be ordered alphabetically by category_name and product_name
 
+select category_name, product_name
+from products
+join categories using (category_id)
+where product_name like 'S%'
+order by category_name, product_name;
