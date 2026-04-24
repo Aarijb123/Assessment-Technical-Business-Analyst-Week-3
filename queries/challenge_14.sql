1a2393c1e573122b318af03aefd7344232ff1b02
Aarij <aarijb@outlook.com>
2026-04-24 15:58:53 +0100

    chllenge 14

--- queries/challenge_14.sql
+++ queries/challenge_14.sql
@@ -4,3 +4,10 @@
 
 -- Rows should be sorted in ascending order by the date of the customer's earliest order
 
+CREATE OR REPLACE VIEW customer_summary AS
+SELECT c.company_name AS company, c.city, COUNT(o.order_id) AS total_orders
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+GROUP BY c.customer_id
+HAVING COUNT(o.order_id) > 15
+ORDER BY MIN(o.order_date) ASC;
