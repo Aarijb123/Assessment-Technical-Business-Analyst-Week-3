0f160558a5bfebb470b90d69e6a95a7d90c035d1
Aarij <aarijb@outlook.com>
2026-04-24 15:54:18 +0100

    challenge 13

--- queries/challenge_13.sql
+++ queries/challenge_13.sql
@@ -10,3 +10,10 @@
 
 -- Territory lists should be lists, not concatenated strings
 
+select concat(first_name, ' ', last_name) as employee, array_agg(territory_description order by territory_description) as assigned_territories
+from employees
+join employee_territories using (employee_id)
+join territories using (territory_id)
+where territory_description not like '% %'
+group by employee_id
+order by count(territory_id) desc;      
