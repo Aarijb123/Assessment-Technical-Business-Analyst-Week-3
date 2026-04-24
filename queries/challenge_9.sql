4bfe96308f9a2ddd7ddf178b7594b63ea66384a9
Aarij <aarijb@outlook.com>
2026-04-24 15:27:36 +0100

    challenge 9

--- queries/challenge_9.sql
+++ queries/challenge_9.sql
@@ -2,3 +2,6 @@
 
 -- Results should be ordered in descending order of employment year, with ties broken by sorting alphabetically by last name (ascending).
 
+select concat(title_of_courtesy, ' ', first_name, ' ', last_name) as employee_full_name
+from employees
+order by extract(year from hire_date) desc, last_name asc;
